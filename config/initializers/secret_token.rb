5c9e0ccb6440a42c1c594f94b08719ddcb28f73a
Persa Zula <persa@persazula.com>
2014-02-19 14:52:29 -0700

    Setting up Rails to take geospatial data

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refuelly::Application.config.secret_key_base = 'ae4f2d40439d05dd4f9e44a27fd28afb24332675f722623ed06f44771ee5fffc1239e76d60a0c53940e4e496b54c6ba4058a7526d7b38cb57067d459959cfeca'
